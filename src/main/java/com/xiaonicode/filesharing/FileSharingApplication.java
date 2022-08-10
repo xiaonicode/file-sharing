819db99112090af7ff54cf61517b00db6efe5147
xiaonicode <89631642+xiaonicode@users.noreply.github.com>
2022-08-10 17:05:00 +0800

    feat: initial commit

---
+++ src/main/java/com/xiaonicode/filesharing/FileSharingApplication.java
@@ -0,0 +1,19 @@
+package com.xiaonicode.filesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目主启动类
+ *
+ * @author xiaonicode
+ * @createTime 2022-08-10
+ */
+@SpringBootApplication
+public class FileSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSharingApplication.class, args);
+    }
+
+}
